02169c5b0b80e7428b8ca1c092a14962e04fbb73
chadswen <chadswen@gmail.com>
2011-03-27 02:20:39 +0000

    Massive update! lots of things changed. 1. Accelerometer responds accurately to shakes. 2. Hold to roll button eliminated, rolling stops after no movement detected for 3 seconds. 3. Random numbers are spawned at  rate according to the component velocity. 4. Roll results are stored in the global variable rollResults: a qvariant that holds a 2D javascript array 5. Tons of random stuff.

--- DiceHero/qml/globalDice.js
+++ DiceHero/qml/globalDice.js
@@ -1,1 +1,10 @@
 var numDice = new Array(6);
+
+var rollResults = new Array(6);
+//up to 30 of each die type
+rollResults[0] = []
+rollResults[1] = []
+rollResults[2] = []
+rollResults[3] = []
+rollResults[4] = []
+rollResults[5] = []
